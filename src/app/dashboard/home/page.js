c77f1d07a1ddf5f0f98210b070bd1799d3e2b413
Saied-AbdAlzaker <saiedabdalzaker@gmail.com>
2025-02-01 14:15:44 +0200

    price style in home page

--- src/app/dashboard/home/page.js
+++ src/app/dashboard/home/page.js
@@ -12,7 +12,7 @@ export default async function HomePage() {
       <div className="p-5">
         <div className="grid grid-col gap-8 lg:grid-cols-4 lg:gap-6">
           {products.slice(0, 4).map((product) => (
-            <div className="p-3 shadow">
+            <div className="p-3 shadow relative">
               <img
                 alt
                 src={product.image}
